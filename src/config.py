8926d3d64fcd3605d04f7e5ac3b70810e7bec590
meendragon <kimhy1124@soongsil.ac.kr>
2025-06-09 17:26:26 +0900

    Update config.py

--- src/config.py
+++ src/config.py
@@ -1,4 +1,5 @@
 # config.py
+import torch.cuda
 
 class BaseConfig:
     image_size = (224, 224)
@@ -71,4 +72,30 @@ class AgeRegEvalConfig(BaseConfig):
     feature_size = 512          # VCOP에서 사용된 encoder output dim과 동일해야 함
     tuple_len = 4
 
+config = {
+    'img_width': 128,
+    'img_height': 128,
+    'img_size': 128,
+    'mean': [0.485, 0.456, 0.406],
+    'std': [0.229, 0.224, 0.225],
+    'model_name': 'resnet',
+    'root_dir': '',
+    'csv_path': '',
+    'device': 'cuda' if torch.cuda.is_available() else 'cpu',
+    'image_path_test_folder': './megaage_asian/test',
+    'leaky_relu': False,
+    'epochs': 20,
+    'batch_size': 128,
+    'eval_batch_size': 256,
+    'seed': 42,
+    'lr': 0.0001,
+    'wd': 0.001,
+    'save_interval': 1,
+    'reload_checkpoint': None,
+    'finetune': 'weights/FA_DOCS/crnn-fa-base.pt',
+    # 'finetune': None,
+    'weights_dir': 'weights',
+    'log_dir': 'logs',
+    'cpu_workers': 0,
+}
 
